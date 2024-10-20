145324ee5e0b99f9c88eacc03717ba7df606d160
Mahesh0111m <maheshm0111mhm@gmail.com>
2024-10-20 21:50:09 +0530

    updated

--- src/main/java/com/example/demo/repo/RegistrationRepository.java
+++ src/main/java/com/example/demo/repo/RegistrationRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repo;
 
 import com.example.demo.model.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ResponseBody
+@Repository
 public interface RegistrationRepository extends JpaRepository<Registration,Integer> {
 
 }
